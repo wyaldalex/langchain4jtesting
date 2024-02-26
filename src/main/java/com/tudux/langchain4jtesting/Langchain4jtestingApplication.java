dd152f63676dec0c37af85726d2a7dbad791517d
Alexander Alvarenga <moltron_nano@hotmail.com>
2024-02-25 22:04:33 -0600

    Initial commit

---
+++ src/main/java/com/tudux/langchain4jtesting/Langchain4jtestingApplication.java
@@ -0,0 +1,13 @@
+package com.tudux.langchain4jtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchain4jtestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tudux/langchain4jtesting/Langchain4jtestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudux.langchain4jtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jtestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
